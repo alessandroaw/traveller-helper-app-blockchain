bd2c220272e27b3071ba883719a79c00d58b8804
Sandro <assandroa95@gmail.com>
2020-12-10 16:09:14 +0700

    adding environment variable

--- api/src/db/mongoose.js
+++ api/src/db/mongoose.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const MONGODB_URI = "mongodb://jeffry:jeffry123@ds035673.mlab.com:35673/traveller-helper";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true
-});
+}).then(() => { console.log('success!! connected to database') });
 
 module.exports = mongoose;
