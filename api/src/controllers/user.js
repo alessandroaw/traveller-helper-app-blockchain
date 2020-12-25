9d598f13406dae28e9af53f71f7a4506e9e78961
Jeffry Khosasi <jeffry@riset.ai>
2020-12-25 14:38:59 +0700

    adding user functionalities by adding handler at controller and routes

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -22,5 +22,48 @@ async function login(req, res, next) {
     })
 }
 
+async function getUser(req, res, next) {
+    try {
+        const user = await User.find({ethereumAddress: req.params.ethereumAddress});
+        res.status(200).send(user);
+    } catch(e) {
+        console.error(e);
+        res.status(400).send(e);
+    }
+}
 
-module.exports = {login}
+async function updateUserBalance(req, res, next) {
+    const { id } = req.params;
+    try {
+        const updatedBalance = await User.findByIdAndUpdate(id, {$set: {etherBalance: req.body.etherBalance}});
+        res.send(updatedBalance);
+    } catch(e) {
+        console.error(e);
+        res.status(400).send(e);
+    }
+}
+
+async function userToTraveller(req, res, next) {
+    const { id } = req.params;
+    try {
+        const travellerManagerAddress = await User.findByIdAndUpdate(id, {$set: {travellerManagerAddress: req.body.travellerManagerAddress}});
+        res.send(travellerManagerAddress);
+    } catch(e) {
+        console.error(e);
+        res.status(400).send(e);
+    }
+}
+
+async function travellerDeposit(req, res, next) {
+    const { id } = req.params;
+    try {
+        const travellerDepositEth = await User.findByIdAndUpdate(id, {$set: {travellerDeposit: req.body.travellerDeposit}});
+        res.send(travellerDepositEth);
+    } catch(e) {
+        console.error(e);
+        res.status(400).send(e);
+    }
+}
+
+
+module.exports = {login, getUser, updateUserBalance, userToTraveller, travellerDeposit}
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -4,5 +4,10 @@ const userController = require('../controllers/user');
 const router = express.Router()
 
 router.post('/login', userController.login);
+router.post('/updateBalance', userController.updateUserBalance);
+router.post('/beTraveller', userController.userToTraveller);
+router.post('/travellerDeposit', userController.travellerDeposit);
+
+router.get('/user', userController.getUser);
 
 module.exports = router;
